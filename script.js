c236835232597569bf4dc129cf11cc65c07213ab
mazlum414 <elonmuskoruspusu@gmail.com>
2025-02-01 21:43:20 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function addComment() {
-    let comment = document.getElementById('comment').value;
-    if (comment.trim() !== "") {
-        let commentBox = document.createElement("p");
-        commentBox.innerText = comment;
-        document.getElementById('comments').appendChild(commentBox);
-        document.getElementById('comment').value = "";
-    }
-}
